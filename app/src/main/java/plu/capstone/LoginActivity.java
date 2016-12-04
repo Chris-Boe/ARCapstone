3a36519f8bbbb29c723900cdaf9c184fcb9dd82e
Chris Boe <cboe14@yahoo.com>
2016-12-03 17:21:00 -0800

    testcomment

--- app/src/main/java/plu/capstone/LoginActivity.java
+++ app/src/main/java/plu/capstone/LoginActivity.java
@@ -34,7 +34,7 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-/**
+/** comment
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {

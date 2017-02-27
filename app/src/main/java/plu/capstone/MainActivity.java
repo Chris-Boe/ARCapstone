abe4d938f00931ba90ab8a4bf88190f7cdb8ec35
Chris Boe <Chris Boe>
2017-02-26 23:20:26 -0800

    Added GPS Toast to main activity

--- app/src/main/java/plu/capstone/MainActivity.java
+++ app/src/main/java/plu/capstone/MainActivity.java
@@ -1,11 +1,20 @@
 package plu.capstone;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import android.location.LocationListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,37 +22,43 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
     //private HashMap<String, ArrayList<String>> map;
     //Blah blah blah test comment again
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Button mapButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         FloatingActionButton camButton = (FloatingActionButton) findViewById(R.id.camButton);
-        camButton.setOnClickListener(new View.OnClickListener(){
+        camButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 goToCamView();
             }
         });
         Button calButton = (Button) findViewById(R.id.calButton);
-        calButton.setOnClickListener(new View.OnClickListener(){
+        calButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 goToCal();
             }
         });
         Button myEventsButton = (Button) findViewById(R.id.myEventsButton);
-        myEventsButton.setOnClickListener(new View.OnClickListener(){
+        myEventsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 goToMyEvents();
             }
         });
-        Button mapButton = (Button)findViewById(R.id.mapButton);
-        mapButton.setOnClickListener(new View.OnClickListener(){
+        mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-
+            public void onClick(View v) {
+                toastLoc();
+                //Toast toast = Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_SHORT);
+                //toast.show();
                 goToMap();
             }
         });
@@ -52,6 +67,55 @@ public class MainActivity extends AppCompatActivity {
         //HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
         //RSSReader reader = new RSSReader("https://25livepub.collegenet.com/calendars/all.rss");
         //map = reader.getMap();
+    }
+
+    private void toastLoc() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                String printLoc = "Lat: " + location.getLatitude() + " Long: " + location.getLongitude();
+                Toast toast = Toast.makeText(getApplicationContext(), printLoc, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        android.Manifest.permission.INTERNET
+                }, 10);
+                return;
+            } else {
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    toastLoc();
+                return;
+        }
     }
     private void goToCamView(){
         Intent intent = new Intent(this, CameraActivity.class);
